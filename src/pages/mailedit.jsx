70b0e395cbe697c69f374af60a570b20ebef6b80
maruhashiyukina <sachiyuki14@icloud.com>
2023-09-16 18:23:41 +0900

    yukiすごいやろーてんさいやわ

--- src/pages/mailedit.jsx
+++ src/pages/mailedit.jsx
@@ -1,17 +1,39 @@
-
+import React, { useState } from "react";
 
 export default function Mailedit() {
+    const [text, setText] = useState("yukina");
+    const [judge, setJudge] = useState(true);
+    const [buttoncolor, setColor] = useState("#C5E99B");
+
+    const copyText = async () => {
+        await global.navigator.clipboard.writeText(text)
+        window.alert("copied!")
+    }
+
+    const onHandleSetText = (e) => {
+        setText(e.target.value)
+    }
+
+    const pushEdit = () => {
+        setJudge(!judge)
+        if (buttoncolor == "#C5E99B") {
+            setColor("#D9D9D9")
+        } else {
+            setColor("#C5E99B")
+        }
+    }
+
     return (
-        <div style={{width: '100%', height: "100vh"}}>
-            <div style={{ background: '#8cd790'}}>
+        <div style={{ width: '100%', height: "100vh" }}>
+            <div style={{ background: '#8cd790' }}>
                 <div style={{ color: 'white', fontSize: 38, fontWeight: "lighter", letterSpacing: 5, textAlign: 'center' }}>就活メールつくるくん</div>
             </div>
-            <img src="/logo_undo.svg" style={{height: 50}}></img>
-            <div style={{background: 'lightgray', width: '70%', margin: 'auto', marginTop: 35, height: '60%', overflow: 'scroll', position: 'relative'}} className="scroll-mail"><div style={{width: '95%', float: 'left'}}>aaa</div><img src="/logo_copy.svg" style={{width: '5%', position: 'absolute', bottom: 10, right: 10}}></img></div>
-            <div style={{float: "left", marginTop: 60, width: '100%', textAlign: 'center'}}>
-                <button style={{border: 'none', letterSpacing: 3, paddingLeft: 50, paddingRight: 50, paddingTop: 15, paddingBottom: 15, fontSize: 20, borderRadius: 10, background: '#C5E99B', color:'white', marginRight: 200, fontWeight: 'bold'}}>編集</button>
-                <button style={{border: 'none', letterSpacing: 3, paddingLeft: 50, paddingRight: 50, paddingTop: 15, paddingBottom: 15, fontSize: 20, borderRadius: 10, background: '#C5E99B', color:'white', marginLeft: 200, fontWeight: 'bold'}}>送信</button>
+            <img src="/logo_undo.svg" style={{ height: 50 }}></img>
+            <div style={{ background: 'lightgray', width: '70%', margin: 'auto', marginTop: 35, height: '60%', overflow: 'scroll', position: 'relative' }} className="scroll-mail"><div style={{ width: '95%', height: '100%', float: 'left' }}><textarea disabled={judge} style={{ width: '100%', height: '100%', border: 'none', resize: 'none', background: 'lightgray', outline: 'none' }} onChange={onHandleSetText}>{text}</textarea></div><img src="/logo_copy.svg" style={{ width: '5%', position: 'absolute', bottom: 10, right: 10 }} onClick={copyText}></img></div>
+            <div style={{ float: "left", marginTop: 60, width: '100%', textAlign: 'center' }}>
+                <button style={{ border: 'none', letterSpacing: 3, paddingLeft: 50, paddingRight: 50, paddingTop: 15, paddingBottom: 15, fontSize: 20, borderRadius: 10, background: buttoncolor, color: 'white', marginRight: 200, fontWeight: 'bold' }} onClick={pushEdit}>編集</button>
+                <button style={{ border: 'none', letterSpacing: 3, paddingLeft: 50, paddingRight: 50, paddingTop: 15, paddingBottom: 15, fontSize: 20, borderRadius: 10, background: "#C5E99B", color: 'white', marginLeft: 200, fontWeight: 'bold' }}>送信</button>
             </div>
-        </div>
+        </div >
     )
 }
