0ac504e63f295fd123697689bea66cc7a6256622
Wakanaaaaa <126571494+Wakanaaaaa@users.noreply.github.com>
2023-09-17 23:10:58 +0900

    createのcenter追加

--- src/components/create.jsx
+++ src/components/create.jsx
@@ -9,13 +9,9 @@ export default function Create(props) {
 
     return (
         <>
-                <button onClick={createClick} style={{ border: 'none', letterSpacing: 3, paddingLeft: 50, paddingRight: 50, paddingTop: 15, paddingBottom: 15, fontSize: 20, borderRadius: 10, background: "#C5E99B", color: 'white', marginLeft: 200, fontWeight: 'bold', cursor: 'pointer' }}>生成</button>
-
+            <div style={{ width: '100%', textAlign: 'center', marginTop: '10%' }}>
+                <button onClick={createClick} style={{ paddingLeft: 50, paddingRight: 50, paddingTop: 15, paddingBottom: 15, border: 'none', background: "#C5E99B", color: 'white', cursor: 'pointer' }}>生成</button>
             </div>
-        </div> */}
-        <div style={{ width: '100%', textAlign:'center', marginTop: '10%' }}>
-        <button onClick={createClick} style={{paddingLeft: 50, paddingRight: 50, paddingTop: 15, paddingBottom: 15, border: 'none', background: "#C5E99B", color: 'white', cursor: 'pointer'}}>生成</button>
-        </div>
 
 
         </>
