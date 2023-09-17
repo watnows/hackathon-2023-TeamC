043c3b1d77a04adb06554d3a694298524e73611e
Wakanaaaaa <126571494+Wakanaaaaa@users.noreply.github.com>
2023-09-17 12:14:14 +0900

    謝罪と辞退のデザイン

--- src/components/create.jsx
+++ src/components/create.jsx
@@ -2,7 +2,7 @@ export default function Create() {
     return (
         <div style={{ width: '100%' }}>
             <div style={{ width: '50%', margin: 'auto', marginTop: '10%' }}>
-                <button style={{ border: 'none', letterSpacing: 3, paddingLeft: 50, paddingRight: 50, paddingTop: 15, paddingBottom: 15, fontSize: 20, borderRadius: 10, background: "#C5E99B", color: 'white', marginLeft: 200, fontWeight: 'bold' }}>生成</button>
+                <button style={{ border: 'none', letterSpacing: 3, paddingLeft: 50, paddingRight: 50, paddingTop: 15, paddingBottom: 15, fontSize: 20, borderRadius: 10, background: "#C5E99B", color: 'white', marginLeft: 200, fontWeight: 'bold', cursor: 'pointer' }}>生成</button>
 
             </div>
         </div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,22 +5,37 @@ import Create from "@/components/create"
 import YourNameInput from "../components/yourNameInput"
 import Square from "../components/square"
 import Date from "../components/date"
+import Apology from "../components/apology"
+import Refusal from "../components/refusal"
 
 export default function Home() {
     return (
-        <div style={{width: '100vw', height: '100vh', display: 'flex', alignItems: 'stretch'}}>
+        <div style={{ width: '100vw', height: '100vh', display: 'flex', alignItems: 'stretch' }}>
             <SideBar />
 
-            <div style={{width: '85%'}}>
-            <Header  />
-            <Frame />
-            <YourNameInput />
-            <Date />
+            <div style={{ width: '85%' }}>
 
-            <Create />
+                <Header />
+                {/* <div>
+                    <Frame />
+                    <YourNameInput />
+                    <Date />
+                </div> */}
+{/* 
+                <div>
+                    <Frame />
+                    <YourNameInput />
+                    <Apology />
+                </div> */}
+                
+                <div>
+                    <Frame />
+                    <YourNameInput />
+                    <Refusal />
+                </div>
 
+                <Create />
             </div>
-
             <Square />
         </div>
     )
