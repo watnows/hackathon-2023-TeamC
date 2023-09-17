f647d16a72c605dc201df6bc31e6027eb10490b7
Wakanaaaaa <126571494+Wakanaaaaa@users.noreply.github.com>
2023-09-18 02:11:44 +0900

    午前2時踏切にPCを担いでった

--- src/pages/mailedit.jsx
+++ src/pages/mailedit.jsx
@@ -25,13 +25,13 @@ const StyledTextArea=styled.textarea`
 export default function Mailedit() {
     const [text, setText] = useState("mail");
     const [judge, setJudge] = useState(true);
-    const [buttoncolor, setColor] = useState("#C5E99B");
+    const [buttoncolor, setColor] = useState("#77af9c");
 
     const copyText = async () => {
         await global.navigator.clipboard.writeText(text)
         
         if(buttoncolor == "#D9D9D9") {
-            setColor("#C5E99B")
+            setColor("#77af9c")
         }
         if(judge == false){
             setJudge(true)
@@ -46,10 +46,10 @@ export default function Mailedit() {
 
     const pushEdit = () => {
         setJudge(!judge)
-        if (buttoncolor == "#C5E99B") {
+        if (buttoncolor == "#77af9c") {
             setColor("#D9D9D9")
         } else {
-            setColor("#C5E99B")
+            setColor("#77af9c")
         }
     }
 
@@ -59,17 +59,18 @@ export default function Mailedit() {
 
     return (
         <div style={{ width: '100%', height: "100vh" }}>
-            <div style={{ background: '#8cd790' }}>
-                <div style={{ color: 'white', fontSize: 38, fontWeight: "lighter", letterSpacing: 5, textAlign: 'center' }}>就活メールつくるくん</div>
-            </div>
+        <div style={{ width: '100%', height: '10%', float: 'right', background: '#8CD790', border: 'dashed 5px #2e8b57'}}>
+        <link href="https://fonts.googleapis.com/earlyaccess/nicomoji.css" rel="stylesheet"></link>
+        <div style={{ color: 'white', fontSize: 45, textAlign: 'center', letterSpacing: 8, fontFamily: 'Nico Moji', marginTop: 5 }}>シューカツメールつくるくん</div>
+        </div>
             <img onClick={backClick} src="/logo_undo.svg" style={{ height: 50 }}></img>
             <div style={{width: '70%', margin: 'auto', marginTop: 35, height: '60%', position: 'relative' }} className="scroll-mail"><div style={{ width: '95%', height: '100%', float: 'left' }}>
                 <StyledTextArea disabled={judge} onChange={onHandleSetText} value={text}/></div>
                 <img src="/logo_copy.svg" style={{ width: '5%', position: 'absolute', bottom: 0, right: 0}} onClick={copyText}></img>
             </div>
             <div style={{ float: "left", marginTop: 60, width: '100%', textAlign: 'center' }}>
-                <button style={{ border: 'none', letterSpacing: 3, paddingLeft: 50, paddingRight: 50, paddingTop: 15, paddingBottom: 15, fontSize: 20, borderRadius: 10, background: buttoncolor, color: 'white', marginRight: 200, fontWeight: 'bold' }} onClick={pushEdit}>編集</button>
-                <button style={{ border: 'none', letterSpacing: 3, paddingLeft: 50, paddingRight: 50, paddingTop: 15, paddingBottom: 15, fontSize: 20, borderRadius: 10, background: "#C5E99B", color: 'white', marginLeft: 200, fontWeight: 'bold' }}>送信</button>
+                <button style={{ border: 'none', letterSpacing: 3, paddingLeft: 50, paddingRight: 50, paddingTop: 15, paddingBottom: 15, fontSize: 20, borderRadius: 10, background: buttoncolor, color: 'white', marginRight: 200, fontWeight: 'bold', border: 'solid 3px #2e8b57' }} onClick={pushEdit}>編集</button>
+                <button style={{ border: 'none', letterSpacing: 3, paddingLeft: 50, paddingRight: 50, paddingTop: 15, paddingBottom: 15, fontSize: 20, borderRadius: 10, background: "#77af9c", color: 'white', marginLeft: 200, fontWeight: 'bold', border: 'solid 3px #2e8b57' }}>送信</button>
             </div>
         </div >
     )
